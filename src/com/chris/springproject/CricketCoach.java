67407b7e6e4ee133c84dd6de9edf4e82b226a62a
chris0512 <kyang3200@gmail.com>
2022-05-19 15:35:14 +0800

    test the setter methods

--- src/com/chris/springproject/CricketCoach.java
+++ src/com/chris/springproject/CricketCoach.java
@@ -3,6 +3,20 @@ package com.chris.springproject;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	
+	// create a no-arg constructor
+	public CricketCoach() {
+		System.out.println("CricketCoach: inside no-arg constructor");
+	}
+	
+	
+	// our setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: inside setter method - setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+
+
 
 	@Override
 	public String getDailyWorkout() {
