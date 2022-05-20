1ad19920a21aab88efe13e4f290fbd248fb0c4e5
chris0512 <kyang3200@gmail.com>
2022-05-20 14:31:09 +0800

    singleton with same bean

--- src/com/chris/springproject/BeanScopeDemoApp.java
+++ src/com/chris/springproject/BeanScopeDemoApp.java
@@ -1,9 +1,31 @@
 package com.chris.springproject;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class BeanScopeDemoApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		// check if they are same bean
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the result
+		System.out.println("\nPointing to the same object: " + result);
+		
+		System.out.println("\nMemory location for theCoach" + theCoach);
+		
+		System.out.println("\nMemory location for alphaCoach" + alphaCoach);
+		
+		context.close();
 
 	}
 
